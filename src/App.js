61823979609fe8ef044dfd23b826d4c138854255
Bektemir040404 <almazbekovb53@gmail.com>
2022-08-04 14:54:17 +0600

    PromarketRoute

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState([])
+
+  const func = () => {
+    const pp = prompt('Досунун аты ким?')
+    setName( p => [...p, pp]
+    )
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>To do list ({name.length})</h1>
+    <button onClick={func}>Add friends</button> 
+    <ul>{name.map(Element =>{
+      return <li>{Element} ({Element.length})</li>
+    })}</ul>
+    </>
   );
 }
 
